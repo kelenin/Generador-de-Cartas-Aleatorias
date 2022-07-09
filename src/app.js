fefc9bc39539f6edf3dd266ac820900cff7ea891
Katheren Salom <katherensalom@gmail.com>
2022-07-09 20:34:53 +0000

    se creo el codigo aleatorio de las cartas en el app.js solo se dejo class card en el html se mejoro el codigo en el css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,37 @@
 import "./style.css";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(GenerarCartas());
+  document.querySelector(".card").innerHTML = GenerarRandomNumber();
+};
+
+let GenerarRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q"
+  ];
+
+  let Indexnumb = Math.floor(Math.random() * numbers.length);
+
+  return numbers[Indexnumb];
+};
+
+let GenerarCartas = () => {
+  let carts = ["spade", "club", "heart", "diamond"];
+
+  let Indexcarst = Math.floor(Math.random() * carts.length);
+
+  return carts[Indexcarst];
 };
